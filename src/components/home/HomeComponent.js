e2253c69e57e4b6cac7e35ad5bb5d4c4df7827f1
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-05-07 08:42:08 +0200

    add meteo component and change css font

--- src/components/home/HomeComponent.js
+++ src/components/home/HomeComponent.js
@@ -1,8 +1,9 @@
 import { Component } from '../global';
+import { Meteo } from './Meteo';
 import { News } from './News';
 
 export class HomeComponent extends Component {
 	constructor() {
-		super('div', [], new News());
+		super('div', [], [new News(), new Meteo()]);
 	}
 }
--- src/components/home/Meteo.js
+++ src/components/home/Meteo.js
@@ -1,7 +1,15 @@
 import { Component } from '../global';
-
+//const APIKEY = d0f0478fb759f869cb291add389c4369;
 export class Meteo extends Component {
 	constructor() {
-		super();
+		super('div', { name: 'class', value: 'meteo' }, [
+			new Component('h1', { name: 'id', value: 'temperature' }, '10° C'),
+			new Component('img', [
+				{ name: 'src', value: '' },
+				{ name: 'alt', value: 'temps' },
+				{ name: 'class', value: 'meteo-image' },
+			]),
+			new Component('h3', { name: 'id', value: 'date-meto' }, '05/05'),
+		]);
 	}
 }
