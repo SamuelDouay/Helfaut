4596cdcbb159cd4138a8bd66edc08ba3947fd615
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-05-09 11:23:11 +0200

    begin header && finish darck mode

--- src/main.js
+++ src/main.js
@@ -20,7 +20,51 @@ function handleEventLog(e = null) {
 			: new ConnexionButton();
 	container.innerHTML = components.render();
 	components.initEvent();*/
+
+	const light_darck = document.querySelector('#theme_l_d');
+	let darck = false;
+	light_darck.addEventListener('click', e => {
+		e.preventDefault();
+		if (!darck) {
+			darckMode();
+			darck = true;
+		} else {
+			lightMode();
+			darck = false;
+		}
+	});
 }
 
 handleEventLog();
 window.addEventListener('log', handleEventLog);
+
+function darckMode() {
+	const color_darck = ['#212121', '#181818', '#BABABA', '#fff'];
+	document
+		.querySelector('#theme_l_d')
+		.setAttribute('src', '/images/moon_light.svg');
+	document
+		.querySelector('#home_header')
+		.setAttribute('src', '/images/home_white.svg');
+	document.querySelectorAll('nav img').forEach(e => {
+		e.setAttribute('background', '#fff');
+	});
+	document.documentElement.style.setProperty('--color-main', color_darck[0]);
+	document.documentElement.style.setProperty('--color-seg', color_darck[1]);
+	document.documentElement.style.setProperty('--color-ter', color_darck[2]);
+	document.documentElement.style.setProperty('--color-font', color_darck[3]);
+}
+
+function lightMode() {
+	const color_light = ['#f1f1f1', '#ffffff', ' #bababa', ' #000'];
+	document
+		.querySelector('#theme_l_d')
+		.setAttribute('src', '/images/moon_darck.svg');
+	document
+		.querySelector('#home_header')
+		.setAttribute('src', '/images/home.svg');
+	document.documentElement.style.setProperty('--color-main', color_light[0]);
+	document.documentElement.style.setProperty('--color-seg', color_light[1]);
+	document.documentElement.style.setProperty('--color-ter', color_light[2]);
+	document.documentElement.style.setProperty('--color-font', color_light[3]);
+}
