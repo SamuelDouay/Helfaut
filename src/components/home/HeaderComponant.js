f81dc5ff343dc48008439c73f364e95dcda0b375
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-05-12 21:01:22 +0200

    add image

--- src/components/home/HeaderComponant.js
+++ src/components/home/HeaderComponant.js
@@ -4,13 +4,9 @@ export class HeaderComponant extends Component {
 	constructor() {
 		super('div', { name: 'id', value: 'header_home_name' }, [
 			new Component('img', [
-				{ name: 'src', value: '/images/home.svg' },
+				{ name: 'src', value: '/images/baniere_helfaut.png' },
 				{ name: 'alt', value: 'logo helfaut' },
 				{ name: 'id', value: 'helfaut_logo_header' },
-			]),
-			new Div({ name: 'id', value: 'welcome' }, [
-				new Component('h1', null, 'Bienvenue à'),
-				new Component('h2', null, 'Helfaut'),
 			]),
 		]);
 	}
