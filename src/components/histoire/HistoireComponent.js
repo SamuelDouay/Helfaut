96987e9326b777f7b9404df0fb2a03d0e55ea409
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-05-13 12:15:20 +0200

    add onglets for 4 pages

--- src/components/histoire/HistoireComponent.js
+++ src/components/histoire/HistoireComponent.js
@@ -1,11 +1,52 @@
-import { Component } from '../global';
+import { Component, Div } from '../global';
 
 export class HistoireComponent extends Component {
 	constructor() {
-		super(
-			null,
-			null,
-			new Component('h1', { name: 'class', value: 'test' }, 'Histoire')
-		);
+		super('div', { name: 'id', value: 'container' }, [
+			new Component('ul', { name: 'id', value: 'onglets_buttons' }, [
+				new Component(
+					'li',
+					null,
+					new Component('a', { name: 'href', value: '#onglet_1' }, 'Histoire')
+				),
+				new Component(
+					'li',
+					null,
+					new Component(
+						'a',
+						{ name: 'href', value: '#onglet_2' },
+						'Album photos'
+					)
+				),
+			]),
+			new Div(
+				[
+					{ name: 'class', value: 'container_onglet_contenue' },
+					{ name: 'id', value: 'onglet_1' },
+					{ name: 'style', value: 'display:block;' },
+				],
+				'Histoire'
+			),
+			new Div(
+				[
+					{ name: 'class', value: 'container_onglet_contenue' },
+					{ name: 'id', value: 'onglet_2' },
+					{ name: 'style', value: 'display:block;' },
+				],
+				'Album photos'
+			),
+		]);
+	}
+
+	setOnglet(id) {
+		document.querySelectorAll('.container_onglet_contenue').forEach(e => {
+			e.style.display = 'none';
+		});
+		document.querySelector(`#${id}`).style.display = 'block';
+	}
+
+	initEvent() {
+		const current = document.location.href.split('#')[1] || 'onglet_1';
+		this.setOnglet(current);
 	}
 }
--- src/components/pratique/PratiqueComponent.js
+++ src/components/pratique/PratiqueComponent.js
@@ -1,11 +1,95 @@
-import { Component } from '../global';
+import { Component, Div } from '../global';
 
 export class PratiqueComponent extends Component {
 	constructor() {
-		super(
-			null,
-			null,
-			new Component('h1', { name: 'class', value: 'test' }, 'Pratique')
-		);
+		super('div', { name: 'id', value: 'container' }, [
+			new Component('ul', { name: 'id', value: 'onglets_buttons' }, [
+				new Component(
+					'li',
+					null,
+					new Component(
+						'a',
+						{ name: 'href', value: '#onglet_1' },
+						'Horraire de la mairie'
+					)
+				),
+				new Component(
+					'li',
+					null,
+					new Component(
+						'a',
+						{ name: 'href', value: '#onglet_2' },
+						'Salle multifonction'
+					)
+				),
+				new Component(
+					'li',
+					null,
+					new Component('a', { name: 'href', value: '#onglet_3' }, 'Services')
+				),
+				new Component(
+					'li',
+					null,
+					new Component('a', { name: 'href', value: '#onglet_4' }, 'Ecoles')
+				),
+				new Component(
+					'li',
+					null,
+					new Component('a', { name: 'href', value: '#onglet_5' }, 'Economie')
+				),
+			]),
+			new Div(
+				[
+					{ name: 'class', value: 'container_onglet_contenue' },
+					{ name: 'id', value: 'onglet_1' },
+					{ name: 'style', value: 'display:block;' },
+				],
+				'horraire de la mairie'
+			),
+			new Div(
+				[
+					{ name: 'class', value: 'container_onglet_contenue' },
+					{ name: 'id', value: 'onglet_2' },
+					{ name: 'style', value: 'display:block;' },
+				],
+				'Salle multifonction'
+			),
+			new Div(
+				[
+					{ name: 'class', value: 'container_onglet_contenue' },
+					{ name: 'id', value: 'onglet_3' },
+					{ name: 'style', value: 'display:block;' },
+				],
+				'Services'
+			),
+			new Div(
+				[
+					{ name: 'class', value: 'container_onglet_contenue' },
+					{ name: 'id', value: 'onglet_4' },
+					{ name: 'style', value: 'display:block;' },
+				],
+				'Ecole'
+			),
+			new Div(
+				[
+					{ name: 'class', value: 'container_onglet_contenue' },
+					{ name: 'id', value: 'onglet_5' },
+					{ name: 'style', value: 'display:block;' },
+				],
+				'Economie'
+			),
+		]);
+	}
+
+	setOnglet(id) {
+		document.querySelectorAll('.container_onglet_contenue').forEach(e => {
+			e.style.display = 'none';
+		});
+		document.querySelector(`#${id}`).style.display = 'block';
+	}
+
+	initEvent() {
+		const current = document.location.href.split('#')[1] || 'onglet_1';
+		this.setOnglet(current);
 	}
 }
