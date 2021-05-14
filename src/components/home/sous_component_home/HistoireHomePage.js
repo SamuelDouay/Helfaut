ec318f0b2b22fdf6d274c760c85830420dbbf9af
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-05-14 06:38:49 +0200

    histoire component

--- src/components/home/sous_component_home/HistoireHomePage.js
+++ src/components/home/sous_component_home/HistoireHomePage.js
@@ -1,7 +1,20 @@
-import { Component } from '../../global';
+import { Component, Div } from '../../global';
 
 export class HistoireHomePage extends Component {
 	constructor() {
-		super('div', { name: 'id', value: 'histoire_homepage' }, 'HISTOIRE');
+		super('div', { name: 'id', value: 'histoire_homepage' }, [
+			new Div(
+				{ name: 'id', value: 'histoire_homepage_text' },
+				'Découvrez le patrimoine histoirique du village...'
+			),
+			new Component(
+				'a',
+				[
+					{ name: 'id', value: 'histoire_homepage_lien' },
+					{ name: 'href', value: '/histoire' },
+				],
+				"Histoire d'Helfaut"
+			),
+		]);
 	}
 }
