89cbd1bb2f38c80422cd3b1544ae77a2afcfaf4d
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-05-14 22:15:22 +0200

    fin recherche homepage

--- src/components/home/sous_component_home/SearchInfo.js
+++ src/components/home/sous_component_home/SearchInfo.js
@@ -2,7 +2,7 @@ import { Component, Div } from '../../global';
 
 export class SearchInfo extends Component {
 	constructor(date, information) {
-		super(null, null, [
+		super('div', null, [
 			new Div({ name: 'class', value: 'info_value' }, information),
 			new Div({ name: 'class', value: 'nom_value' }, date),
 		]);
