9b5dd755390241c822334a940d14a7d59051d4d5
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-05-14 07:08:59 +0200

    chiffre

--- src/components/home/sous_component_home/Chiffre.js
+++ src/components/home/sous_component_home/Chiffre.js
@@ -5,23 +5,27 @@ export class Chiffre extends Component {
 		super('div', { name: 'id', value: 'chiffre_home_page' }, [
 			new Div({ name: 'class', value: 'donnee_value' }, [
 				new Component(
-					'h1',
+					'div',
 					{ name: 'class', value: 'nom_value' },
 					'Superficie'
 				),
 				new Component(
-					'h2',
+					'div',
 					{ name: 'class', value: 'chiffre_value' },
 					'8.2 km²'
 				),
 			]),
 			new Div({ name: 'class', value: 'donnee_value' }, [
 				new Component(
-					'h1',
+					'div',
 					{ name: 'class', value: 'nom_value' },
 					'Nb habitants'
 				),
-				new Component('h2', { name: 'class', value: 'chiffre_value' }, '1 642'),
+				new Component(
+					'div',
+					{ name: 'class', value: 'chiffre_value' },
+					'1 642'
+				),
 			]),
 		]);
 	}
