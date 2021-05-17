aacde228f0b82013210db1191a6a117767acb2e5
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-05-17 23:06:06 +0200

    histoire de la commune

--- src/components/histoire/HistoireComponent.js
+++ src/components/histoire/HistoireComponent.js
@@ -1,11 +1,12 @@
 import { Component } from '../global';
 import { OngletContentList, OngletList } from '../transversal';
+import { HistoireCommune } from './HistoireCommune';
 
 export class HistoireComponent extends Component {
 	constructor() {
 		super('div', { name: 'id', value: 'container' }, [
 			new OngletList(['Histoire', 'Album Photo']),
-			new OngletContentList(['Histoire', 'Album Photo']),
+			new OngletContentList([new HistoireCommune(), 'Album Photo']),
 		]);
 	}
 }
