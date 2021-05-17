67ed811de678f9531e9ef32755de8464e797e1a2
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-05-17 23:06:06 +0200

    histoire de la commune

---
+++ src/components/histoire/HistoireCommune.js
@@ -0,0 +1,68 @@
+import { Component, P } from '../global';
+
+export class HistoireCommune extends Component {
+	constructor() {
+		super('div', { name: 'id', value: 'histoire_commune' }, [
+			new P(
+				null,
+				'Helfaut est un lieu d’implantation de l’Homme depuis les temps les plus reculés. On en trouve des traces dès 40 000 ans avant notre ère (outils d’os ou de silex par exemple).'
+			),
+			new P(
+				null,
+				'Au 14ème siècle, la région est investie par les troupes anglaises, et au 16ème par les Flamands puis par Charles Quint. L’église est incendiée, puis reconstruite.'
+			),
+			new P(
+				null,
+				'En 1598 l’évêque Christophe de France se réfugie à Bilques durant l’épidémie de peste qui décime la région. De même en 1625, sous l’occupation espagnole, la peste décime presque toute la population. Ce n’est qu’en 1644 que quelques habitants reviendront vivre dans des cabanes adossées aux murs de l’église.'
+			),
+			new P(
+				null,
+				'En 1657 l’abbé Louis Palfart est nommé curé d’Helfaut, où il ne reste que 152 habitants, mendiants ou de fortune médiocre selon le codicille de son testament. En 1677 Helfaut est repris par les Français, qui brûlent entièrement le village.'
+			),
+			new P(
+				null,
+				'Le 10 août 1678, au traité de Nimègue, Louis XIV annexe l’audomarois, et donc Helfaut, au royaume de France.'
+			),
+			new P(
+				null,
+				'A la fin du 18ème siècle, un campement de 2 500 militaires s’établit sur le plateau d’Helfaut. Il comptera jusqu’à 20 000 hommes ; le village s’agrandit. Les églises d’Helfaut et de Bilques sont vendues comme biens nationaux. Celle de Bilques est rachetée par un paroissien pour la préserver.'
+			),
+			new P(
+				null,
+				'En 1804, de retour de Boulogne sur Mer, Napoléon 1er assiste à des manœuvres militaires au camp d’Helfaut. Il y fait construire 400 baraquements alignés avec jardins, et une chapelle.'
+			),
+			new P(
+				null,
+				'En 1815, après la chute de l’Empire, des troupes anglaises et écossaises occupent le camp. Les Français le reprendront en 1825. Entre temps, en 1819 et par ordonnance royale, la commune de Bilques est fusionnée avec celle d’Helfaut.'
+			),
+			new P(
+				null,
+				'En 1826, le roi Charles X et le duc Ferdinand Philippe d’Orléans assistent à des manœuvres.au camp d’Helfaut. En 1842 est érigée une colonne en souvenir du duc d’Orléans, fils aîné du roi Louis Philippe, mort accidentellement alors qu’il devait assister à de nouvelles manœuvres, toujours au camp d’Helfaut. En 1853, c’st Napoléon III et l’impératrice Eugénie qui viennent à Helfaut assister, encore, à des manœuvres militaires. C’est le maréchal de Mac Mahon qui présidera aux dernières manœuvres au camp d’Helfaut, en 1874.'
+			),
+			new P(
+				null,
+				'C’est entre 1906 et 1908 que seront construits le groupe scolaire et la mairie. L’ensemble ne sera inauguré qu’en 1911, année durant laquelle sera érigé également le monument à la mémoire de l’abbé Palfart.'
+			),
+			new P(
+				null,
+				'46 Helfallois tomberont pendant la Grande Guerre de 1914 à 1918. Le monument aux morts sera inauguré le 27 juillet 1921.'
+			),
+			new P(
+				null,
+				'En 1931, le département du Pas de Calais fait construire le « sanatorium du camp d’Helfaut », sut 45 hectares de terres boisées.'
+			),
+			new P(
+				null,
+				'Durant la seconde guerre mondiale, de 1939 à 1945, Helfaut déplorera 4 victimes militaires et 23 civiles tuées lors des bombardements, bombardements alliés qui visaient essentiellement le « bunker de Wizernes » construit par l’organisation Todt sous les ordres de Hitler. Ce gigantesque blockhaus devait abriter les V1 puis les V2 que le Reich destinait à bombarder l’Angleterre toute proche. C’est ce bunker que l’on nomme aujourd’hui la Coupole et qui abrite un centre de d’histoire et de mémoire qui attire des touristes du monde entier. En septembre 1943, les troupes britanniques s’emparent de la Coupole, qui n’aura jamais vu de tir de missile à destination de la Grande Bretagne.'
+			),
+			new P(
+				null,
+				'En 1972, le sanatorium est l’objet d’importants travaux, et en 1995 il devient le Centre Hospitalier de la Région de Saint-Omer.'
+			),
+			new P(
+				null,
+				'Crédits : Merci à Roland Dussaussoy au Comité d’Histoire pour le Haut Pays, pour leurs recherches historiques sur le sujet.'
+			),
+		]);
+	}
+}
