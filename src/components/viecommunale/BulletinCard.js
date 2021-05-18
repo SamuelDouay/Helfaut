e2642750d5e6008c952cd375fbe84fd034c272ed
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-05-18 11:44:07 +0200

    debut bulletin

---
+++ src/components/viecommunale/BulletinCard.js
@@ -0,0 +1,22 @@
+import { Component, Div } from '../global';
+
+export class BulletinCard extends Component {
+	constructor({ numero, jour, mois, annee }) {
+		super('div', { name: 'class', value: 'bulletin_card' }, [
+			new Div(null, [
+				new Div(
+					{ name: 'class', value: 'numero_bulletin' },
+					`Bulletin numero ${numero}`
+				),
+				new Div(
+					{ name: 'class', value: 'date_bulletin' },
+					`Le ${jour}/${mois}/${annee}`
+				),
+			]),
+			new Component('img', [
+				{ name: 'src', value: '/images/telecharger.svg' },
+				{ name: 'alt', value: 'telechager' },
+			]),
+		]);
+	}
+}
---
+++ src/components/viecommunale/Bulletins.js
@@ -0,0 +1,22 @@
+import { Component } from '../global';
+import { BulletinAffichageList } from './BulletinAffichageList';
+
+export class Bulletins extends Component {
+	constructor() {
+		super('div', { name: 'id', value: 'bulletin_municipaux' }, [
+			new Component(
+				'h1',
+				{ name: 'id', value: 'title_bulletin' },
+				'Découvrez les bulletins municipaux de la commune'
+			),
+			new BulletinAffichageList([
+				{ numero: 'XXX', jour: 'JJ', mois: 'MMM', annee: 'AAAA' },
+				{ numero: 'XXX', jour: 'JJ', mois: 'MMM', annee: 'AAAA' },
+				{ numero: 'XXX', jour: 'JJ', mois: 'MMM', annee: 'AAAA' },
+				{ numero: 'XXX', jour: 'JJ', mois: 'MMM', annee: 'AAAA' },
+				{ numero: 'XXX', jour: 'JJ', mois: 'MMM', annee: 'AAAA' },
+				{ numero: 'XXX', jour: 'JJ', mois: 'MMM', annee: 'AAAA' },
+			]),
+		]);
+	}
+}
