cb4a294b94b57a0beed4118761d0bea002f20b28
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-05-18 07:53:15 +0200

    fin histoire (enfin presque5)

--- src/components/histoire/AlbumPhoto.js
+++ src/components/histoire/AlbumPhoto.js
@@ -3,18 +3,18 @@ import { Component, Div } from '../global';
 export class AlbumPhoto extends Component {
 	constructor() {
 		super('div', { name: 'id', value: 'album_photo' }, [
-			new Div({ name: 'class', value: 'fleche' }, [
-				new Component('img', [
-					{ name: 'src', value: '/images/fleche_gauche.svg' },
-					{ name: 'alt', value: 'fleche' },
-					{ name: 'id', value: 'fleche_de_gauche' },
-				]),
-				new Div({ name: 'id', value: 'photos' }, ''),
-				new Component('img', [
-					{ name: 'src', value: '/images/fleche_droite.svg' },
-					{ name: 'alt', value: 'fleche' },
-					{ name: 'id', value: 'fleche_de_droite' },
-				]),
+			new Component('img', [
+				{ name: 'src', value: '/images/fleche_gauche.svg' },
+				{ name: 'alt', value: 'fleche' },
+				{ name: 'id', value: 'fleche_de_gauche' },
+				{ name: 'class', value: 'fleche' },
+			]),
+			new Div({ name: 'id', value: 'photos' }, ''),
+			new Component('img', [
+				{ name: 'src', value: '/images/fleche_droite.svg' },
+				{ name: 'alt', value: 'fleche' },
+				{ name: 'id', value: 'fleche_de_droite' },
+				{ name: 'class', value: 'fleche' },
 			]),
 		]);
 	}
