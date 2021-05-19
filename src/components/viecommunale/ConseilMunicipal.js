6e127554e70bd8a1fe60e473133074c5ed5a6f45
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-05-19 13:46:56 +0200

    debut conseil

---
+++ src/components/viecommunale/ConseilMunicipal.js
@@ -0,0 +1,7 @@
+import { Component } from '../global';
+
+export class ConseilMunicipal extends Component {
+	constructor() {
+		super('div', { name: 'id', value: 'conseil_container' }, 'test');
+	}
+}
