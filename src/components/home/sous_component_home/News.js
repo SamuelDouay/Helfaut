31be7cc474abacab5e082ae5917300eead242022
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-05-23 21:47:17 +0200

    homepage finish

--- src/components/home/sous_component_home/News.js
+++ src/components/home/sous_component_home/News.js
@@ -4,6 +4,29 @@ export class News extends Component {
 	constructor() {
 		super('div', { name: 'id', value: 'new_homepage' }, [
 			new Div({ name: 'id', value: 'tille_new' }, 'Informations'),
+			new Div({ name: 'id', value: 'news_container' }, [
+				new Div({ name: 'class', value: 'fleches_news' }, '<'),
+				new Div({ name: 'id', value: 'news' }, [
+					new Div({ name: 'class', value: 'title_news_content' }, [
+						new Div({ name: 'class', value: 'date_news' }, '30 avril 2021'),
+						new Component(
+							'h1',
+							{ name: 'class', value: 'title_news_content_t' },
+							'TITRE LOREM IPSUM'
+						),
+						new Component(
+							'a',
+							[
+								{ name: 'id', value: 'homepage_lien' },
+								{ name: 'href', value: '/activites' },
+							],
+							'En savoir plus...'
+						),
+					]),
+					new Div({ name: 'class', value: 'media_content' }, '[Texte ou phto]'),
+				]),
+				new Div({ name: 'class', value: 'fleches_news' }, '>'),
+			]),
 		]);
 	}
 }
