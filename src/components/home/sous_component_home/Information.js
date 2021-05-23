4417792a2ae80c236e88df38c7a96e0a19a7d7f4
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-05-23 21:12:51 +0200

    marie_info

--- src/components/home/sous_component_home/Information.js
+++ src/components/home/sous_component_home/Information.js
@@ -1,7 +1,16 @@
-import { Component } from '../../global';
+import { Component, Div } from '../../global';
 
 export class Information extends Component {
 	constructor() {
-		super('div', { name: 'id', value: 'information_homepage' }, "Mairie d'helfaut");
+		super('div', { name: 'id', value: 'information_homepage' }, [
+			new Div({ name: 'id', value: 'info_title' }, "Maire d'Helfaut"),
+			new Div(
+				{ name: 'id', value: 'addresse_info' },
+				'74 Rue du Pipestraque, </br> 62570 Helfaut'
+			),
+			new Div({ name: 'id', value: 'info_horaire' }, 'Ouvert - Ferme à 17h30'),
+			new Div({ name: 'id', value: 'info_num_tel' }, '03 21 93 80 07'),
+			new Div({ name: 'id', value: 'info_mail' }, 'commune.helfaut@wanadoo.fr'),
+		]);
 	}
 }
