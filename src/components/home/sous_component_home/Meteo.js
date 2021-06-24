477e046af2327260ccc7ae0a7f401619bbd717b8
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-06-24 23:39:49 +0200

    update meteo

--- src/components/home/sous_component_home/Meteo.js
+++ src/components/home/sous_component_home/Meteo.js
@@ -23,12 +23,15 @@ export class Meteo extends Component {
 	}
 
 	handleQueryChange() {
+		const date = new Date();
+		const options = { year: 'numeric', month: 'long', day: 'numeric' };
+
 		this.meteoRessource.getOne().then(rep => {
 			document.querySelector('#meteo_temperature').innerHTML =
 				Math.round(rep.main.temp) + ' °C';
 			document.querySelector('#meteo_image').setAttribute('src',`https://openweathermap.org/img/wn/${rep.weather[0].icon}@2x.png`);
-				;
 		});
+		document.querySelector('#date_meteo').innerHTML = date.toLocaleDateString('fr-FR', options);
 	}
 
 	initEvent() {
