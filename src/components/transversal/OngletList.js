378f04bbaa88b7253ea332052e8b23dca3eee35f
Samuel Douay--Ryckelynck <douaysamuel@gmail.com>
2021-07-05 21:47:49 +0200

    feat/debut onglet

--- src/components/transversal/OngletList.js
+++ src/components/transversal/OngletList.js
@@ -6,17 +6,23 @@ export class OngletList extends Component {
 
 	constructor(ongletList, current) {
 		super('div', { name: 'id', value: 'onglets_buttons' }, [
+			window.matchMedia("(max-width : 400px)").matches ?  new OngletCard({largeur: 10,
+				nbId: -1,
+				value: '<',}, current) : null ,
 			...ongletList.map(
 				(value, index) =>
 					new OngletCard(
 						{
-							largeur: 100 / ongletList.length - 1,
+							largeur: window.matchMedia("(max-width : 400px)").matches ? 10 : 100 / ongletList.length - 1,
 							nbId: index + 1,
 							value: value,
 						},
 						current
 					)
 			),
+			window.matchMedia("(max-width : 400px)").matches ?  new OngletCard({largeur: 10 / ongletList.length - 1,
+				nbId: +1,
+				value: '>',}, current) : null ,
 		]);
 		this.ongletList = ongletList;
 	}
